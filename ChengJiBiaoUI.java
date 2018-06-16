c07adf08b8e3aa68f9f406b6cfeafba37d3ee193
duny31030 <duny31030@126.com>
2018-06-17 04:12:05 +0800

    成绩表

--- ChengJiBiaoUI.java
+++ ChengJiBiaoUI.java
@@ -1,5 +1,11 @@
 package Main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -18,90 +24,68 @@ public class ChengJiBiaoUI extends JFrame
     {  
 
         String[] columnNames =  
-        { "课程数目","课程名称", "学分", "绩点", "分数", "补考" };  //设置JTable的列名 
-        Object[][] obj=new Object[7][7];  
-        for (int i=0;i<7;i++)  
-        {  
-            for(int j=0;j<7;j++)  
-            {  
-                switch (j)  
-                {  
-                case 0:  
-                    obj[0][0] = "              1";
-                    obj[0][1] = "          光纤通信"; 
-                    obj[0][2] = "              2"; 
-                    obj[0][3] = "             1.5"; 
-                    obj[0][4] = "              75"; 
-                    obj[0][5] = "              否"; 
+        { "课程数目","课程名称", "学分", "分数", "绩点", "补考" };  //设置JTable的列名 
+        Object[][] obj=new Object[10][7];  
+//        String[][] obj=new String[10][7];
+        String id = MainUI.getStuid();
+        
+        // 连接数据库
+        try 
+    	{
+        	String url = "jdbc:mysql://localhost:3306/SC";
+        	String user = "root";
+        	String password = "123456";
+			Connection connect = DriverManager.getConnection(url, user, password);
+			Statement stmt = connect.createStatement();
+  	      	ResultSet rs = stmt.executeQuery("select * from Course");
+  	      	int i = 0;
+  	      	while(rs.next())
+  	      	{
+  	      		String getStuid = new String(rs.getString("stu_id"));
+  	      		if(id.equals(getStuid))
+  	      		{
+  	      			obj[i][0] = String.valueOf(i+1);
+  	      			obj[i][1] = new String(rs.getString("cou"));
+  	      			obj[i][2] = new String(rs.getString("sou_sco"));
+  	      			obj[i][3] = new String(rs.getString("score"));
+  	      			int sco = Integer.parseInt(new String(rs.getString("score")));
+  	      			if(sco < 60)
+  	      			{
+  	      				obj[i][4] = "0";
+  	      				obj[i][5] = "是";
+  	      			}
 
-
-                    break;  
-                case 1:  
-                    obj[1][0] = "              2";
-                    obj[1][1] = "          Android"; 
-                    obj[1][2] = "              2"; 
-                    obj[1][3] = "             1.8"; 
-                    obj[1][4] = "              5"; 
-                    obj[1][5] = "              否"; 
-
-                    break;  
-                case 2:  
-                    obj[2][0] = "              3";
-                    obj[2][1] = "          物联网"; 
-                    obj[2][2] = "              1.5"; 
-                    obj[2][3] = "             1.2"; 
-                    obj[2][4] = "              70"; 
-                    obj[2][5] = "              否"; 
-
-                    break;  
-                case 3:  
-                    obj[3][0] = "              4";
-                    obj[3][1] = "          电磁场"; 
-                    obj[3][2] = "              2"; 
-                    obj[3][3] = "             1.6"; 
-                    obj[3][4] = "              78"; 
-                    obj[3][5] = "              否"; 
-
-                    break;  
-                case 4:  
-                    obj[4][0] = "              5";
-                    obj[4][1] = "         手机维修"; 
-                    obj[4][2] = "              1.0"; 
-                    obj[4][3] = "             1.0"; 
-                    obj[4][4] = "              优秀"; 
-                    obj[4][5] = "              否"; 
-
-                    break;  
-                case 5:  
-                    obj[5][0] = "              6";
-                    obj[5][1] = "             DSP"; 
-                    obj[5][2] = "             1.0"; 
-                    obj[5][3] = "             1.0"; 
-                    obj[5][4] = "              良好"; 
-                    obj[5][5] = "              否"; 
-                    break;  
-                case 6:  
-                    obj[6][0] = "              7";
-                    obj[6][1] = "       通信原理实验"; 
-                    obj[6][2] = "             1.0"; 
-                    obj[6][3] = "             1.0"; 
-                    obj[6][4] = "              良好"; 
-                    obj[6][5] = "              否"; 
-                    break;  
-
-                }  
-            }  
-        }  
-
+  	      			else
+  	      			{
+  	      				int t1 = sco/10-5;
+  	      				int t2 = sco%10 > 5 ? 5 : 0;
+  	      				String temp = String.valueOf(t1)+"."+String.valueOf(t2);
+  	      				obj[i][4] = temp;
+  	      				obj[i][5] = "否";
+  	      			}
+  	      			++i;
+  	      		}	
+  	      	}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+ 
         JTable table=new JTable(obj, columnNames);  //JTable的其中一种构造方法 
         TableColumn column=null;                    //设置JTable的列默认的宽度和高度 
         int colunms = table.getColumnCount();  
+        
+        
+        
+        
         for(int i=0;i<colunms;i++)  
         {  
             column = table.getColumnModel().getColumn(i);  
             column.setPreferredWidth(100);          //将每一列的默认宽度设置为100
         }  
-        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF); //设置JTable自动调整列表的状态，此处设置为关闭  
+        //注释掉  否侧显示不全 需要自己拉进度条
+//        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF); //设置JTable自动调整列表的状态，此处设置为关闭  
         JScrollPane scroll = new JScrollPane(table);  //用JScrollPane装载JTable，这样超出范围的列就可以通过滚动条来查看
         scroll.setSize(300, 50);  
 
