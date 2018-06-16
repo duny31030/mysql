5c3120f86b402847ab8265c3c0fc7c3bcea1fdc8
duny31030 <duny31030@126.com>
2018-06-17 04:14:06 +0800

    学生名单页面

--- XueShengMingDanUI.java
+++ XueShengMingDanUI.java
@@ -1,5 +1,11 @@
 package Main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -19,60 +25,31 @@ public class XueShengMingDanUI extends JFrame
         // 设置JTable的列名   
         String[] columnNames =  
         { "学号","姓名",  "专业", "班级"};  
-        Object[][] obj=new Object[7][7];  
-        for (int i=0;i<7;i++)  
-        {  
-            for(int j=0;j<7;j++)  
-            {  
-                switch (j)  
-                {  
-                case 0:  
-                    obj[0][0] = "       14140301";
-                    obj[0][1] = "               王小明"; 
-                    obj[0][2] = "         通信工程"; 
-                    obj[0][3] = "                    2";    
-                    break;  
-                case 1:  
-                    obj[1][0] = "       14140302";
-                    obj[1][1] = "            王雷"; 
-                    obj[1][2] = "         通信工程"; 
-                    obj[1][3] = "                    2"; 
-                    break;  
-                case 2:  
-                    obj[2][0] = "       14140303";
-                    obj[2][1] = "            李芳"; 
-                    obj[2][2] = "         通信工程"; 
-                    obj[2][3] = "                    2"; 
-                    break;  
-                case 3:  
-                    obj[3][0] = "       14140304";
-                    obj[3][1] = "            张三"; 
-                    obj[3][2] = "         通信工程"; 
-                    obj[3][3] = "                    2";                  
-                    break;  
-                case 4:  
-                    obj[4][0] = "       14140305";
-                    obj[4][1] = "            李四"; 
-                    obj[4][2] = "         通信工程"; 
-                    obj[4][3] = "                    2"; 
-                    break;  
-                case 5:  
-                    obj[5][0] = "       14140306";
-                    obj[5][1] = "            文宇"; 
-                    obj[5][2] = "         通信工程"; 
-                    obj[5][3] = "             2"; 
-                    break;  
-                case 6:  
-                    obj[6][0] = "       14140307";
-                    obj[6][1] = "            王飞"; 
-                    obj[6][2] = "         通信工程"; 
-                    obj[6][3] = "                    2";                 
-                    break;  
-
-                }  
-            }  
-        }  
-
+        Object[][] obj=new Object[10][7];  
+        
+        // 连接数据库
+        try 
+    	{
+        	String url = "jdbc:mysql://localhost:3306/SC";
+        	String user = "root";
+        	String password = "123456";
+			Connection connect = DriverManager.getConnection(url, user, password);
+			Statement stmt = connect.createStatement();
+  	      	ResultSet rs = stmt.executeQuery("select * from stu");
+  	      	int i = 0;
+  	      	while(rs.next())
+  	      	{
+      			obj[i][0] = new String(rs.getString("stu_id"));
+      			obj[i][1] = new String(rs.getString("name"));
+      			obj[i][2] = new String(rs.getString("college"));  	 
+      			obj[i][3] = new String(rs.getString("class"));
+      			++i;	
+  	      	}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
         JTable table=new JTable(obj, columnNames);  //JTable的其中一种构造方法 
         TableColumn column=null;                    //设置JTable的列默认的宽度和高度 
         int colunms = table.getColumnCount();  
@@ -80,7 +57,8 @@ public class XueShengMingDanUI extends JFrame
         {  
             column = table.getColumnModel().getColumn(i);  
             column.setPreferredWidth(100);           //将每一列的默认宽度设置为100
-        }   
+        } 
+        
         table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);  //设置JTable自动调整列表的状态，此处设置为关闭                 
         JScrollPane scroll = new JScrollPane(table);  //用JScrollPane装载JTable，这样超出范围的列就可以通过滚动条来查看 
         scroll.setSize(300, 50);         
